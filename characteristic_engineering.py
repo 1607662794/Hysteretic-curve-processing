0de3b968335d6d2b1d82a4181c0c9a55a71a5b8a
憨憨菜 <1607662794@qq.com>
2023-07-24 09:29:27 +0800

    特征工程，只是将力与位移做了乘积，无实际意义

--- characteristic_engineering.py
+++ characteristic_engineering.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+'''该文件用于绘制滞回曲线与累计耗能曲线'''
 # 先导入文件，提取力、位移数据
-InputName = r"E:\研究生\科研生活\滞回曲线实操\数据库-new\Shear_compression_tests\RS1\RS1.txt"
-displace = np.loadtxt(InputName, delimiter='\t', skiprows=1, usecols=0)
-force = np.loadtxt(InputName, delimiter='\t', skiprows=1, usecols=1)
+InputName = r"E:\Code\Hysteretic curve processing\data_new\RS3.csv"
+displace = np.loadtxt(InputName, delimiter=',', skiprows=1, usecols=0)
+force = np.loadtxt(InputName, delimiter=',', skiprows=1, usecols=1)
 print(displace.shape, force.shape)
 # print(type(displace), type(force))
 print(displace)
 print(force)
 
-
 '''下面注释掉的这部分，对于不大规则的滞回曲线处理效果极差，因为有些滞回圈数据极少，甚至只有一个，取每个圈中的最值并不科学'''
 # 分解滞回环，先根据零点(该点与上一个点的乘积为负），将滞回曲线分解成上下部分
 zero_point = []
 zero_number = []
-for i in range(1, len(displace) - 1):#坐标原点不需要对其进行判断
+for i in range(1, len(displace) - 1):  # 坐标原点不需要对其进行判断
     if force[i] * force[i + 1] <= 0:
         zero_point.append(displace[i + 1])
         zero_number.append(i + 1)
@@ -53,11 +53,13 @@ bone_point = []
 bone_number = []
 bone_force = []
 
+
 def reversal_point(point):
     """将对应的点添加到翻转点列表中"""
     bone_number.append(point)
     bone_point.append(displace[point])
     bone_force.append(force[point])
+
 
 for i in range(0, len(displace)):
     if i == 0:
@@ -79,6 +81,7 @@ cumulative_energy = [0]
 cumulative_deformation = [0]
 
 
+# 这种累计耗能计算没有什么意义，全是正值，所以会不断地增减
 def deformation_energy(point):
     for i in range(1, point):  # i为第几个点
         increment_deformation = np.abs(displace[i] - displace[i - 1])
@@ -99,37 +102,49 @@ deformation_energy(len(displace))
 print("累计位移{}".format(cumulative_deformation))
 print("累计能量{}".format(cumulative_energy))
 
-
 # 保存文件
 result = np.array(list(zip(cumulative_deformation, cumulative_energy)))
 # print(np.concatenate((cumulative_deformation,cumulative_energy),axis=0))
-np.savetxt(r"E:\研究生\科研生活\滞回曲线实操\数据库-new\Shear_compression_tests\RS1\characteristic_curve.csv", result, delimiter=",", fmt='%.4f',
+np.savetxt(r"E:\研究生\科研生活\滞回曲线实操\数据库-new\Shear_compression_tests\RS1\characteristic_curve.csv", result,
+           delimiter=",", fmt='%.4f',
            header='cumulative_deformation,cumulative_energy', comments='')
-#csv会自动识别“，”，所以虽然开头没有分开，但最终仍然会被分开
+# csv会自动识别“，”，所以虽然开头没有分开，但最终仍然会被分开
 
 # 绘制可视化图表
 # plt.subplot(1, 2, 1)#两张图放在一起会扭曲，所以我就把两张图直接保存在文件夹了
-plt.xlabel("displace")
-plt.ylabel("force")
-plt.plot(displace, force)
-plt.scatter(bone_point, bone_force, s=80, c='orange', marker='x')
-ax = plt.gca()  # 获取当前坐标的位置
-# 去掉坐标图的上和右 spine翻译成脊梁
-ax.spines['right'].set_color('None')
-ax.spines['top'].set_color('None')
-ax.xaxis.set_ticks_position('bottom')  # 设置bottom为x轴
-ax.yaxis.set_ticks_position('left')  # 设置left为x轴
-ax.spines['bottom'].set_position(('data', 0))  # 这个位置的括号要注意
-ax.spines['left'].set_position(('data', 0))
-plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
-plt.rcParams['axes.unicode_minus'] = False
-plt.title("plot：滞回曲线")
-plt.savefig(r'E:\研究生\科研生活\滞回曲线实操\数据库-new\Shear_compression_tests\RS1\滞回曲线.jpg')
-plt.clf()  # 清空画布
+if False:
+    plt.xlabel("displace")
+    plt.ylabel("force")
+    plt.plot(displace, force)
+    plt.scatter(bone_point, bone_force, s=80, c='orange', marker='x')
+    ax = plt.gca()  # 获取当前坐标的位置
+    # 去掉坐标图的上和右 spine翻译成脊梁
+    ax.spines['right'].set_color('None')
+    ax.spines['top'].set_color('None')
+    ax.xaxis.set_ticks_position('bottom')  # 设置bottom为x轴
+    ax.yaxis.set_ticks_position('left')  # 设置left为x轴
+    ax.spines['bottom'].set_position(('data', 0))  # 这个位置的括号要注意
+    ax.spines['left'].set_position(('data', 0))
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
+    plt.rcParams['axes.unicode_minus'] = False
+    plt.title("plot：滞回曲线")
+    # plt.savefig(r'E:\研究生\科研生活\滞回曲线实操\数据库-new\Shear_compression_tests\RS1\滞回曲线.jpg')
+    # plt.clf()  # 清空画布
 
 # plt.subplot(1, 2, 2)
-plt.plot(cumulative_deformation, cumulative_energy)
-plt.scatter(cumulative_deformation, cumulative_energy, s=20, c='orange', marker='x')
-plt.title("plot:特征曲线")
-plt.savefig(r'E:\研究生\科研生活\滞回曲线实操\数据库-new\Shear_compression_tests\RS1\特征曲线.jpg')
+if True:
+    '''注意这个耗能没有实际意义，只是将横纵坐标增量相乘了'''
+    plt.plot(cumulative_deformation, cumulative_energy)
+    plt.scatter(cumulative_deformation, cumulative_energy, marker='s', edgecolors=['dimgray'])
+    # plt.title("cumulative_energy")
+    plt.title("累计耗能")
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
+    plt.rcParams['axes.unicode_minus'] = False
+    # plt.xlabel("cumulative deformation(mm)")
+    # plt.ylabel("cumulative energy(kN·mm)"
+    plt.xlabel("累计位移(mm)")
+    plt.ylabel("累计耗能(kN·mm)")
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
+plt.rcParams['axes.unicode_minus'] = False
+# plt.savefig(r'E:\研究生\科研生活\滞回曲线实操\数据库-new\Shear_compression_tests\RS1\特征曲线.jpg')
 plt.show()
