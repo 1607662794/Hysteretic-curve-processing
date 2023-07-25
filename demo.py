1369b02e147ab287e5576986aa7916076c0e4f65
憨憨菜 <1607662794@qq.com>
2023-07-25 11:42:00 +0800

    草稿纸

--- demo.py
+++ demo.py
@@ -1,34 +1,21 @@
-import math
+def find_max_abs_force_indices(force, zero_number):
+    max_abs_force_indices = []
 
-def dot_product(vector1, vector2):
-    return sum(a * b for a, b in zip(vector1, vector2))
+    for i in range(len(zero_number) - 1):
+        start_idx = zero_number[i]
+        end_idx = zero_number[i + 1]
+        segment_force = force[start_idx:end_idx]
 
-def vector_length(vector):
-    return math.sqrt(sum(a * a for a in vector))
+        abs_force_values = [abs(val) for val in segment_force]
+        max_abs_force_idx = start_idx + abs_force_values.index(max(abs_force_values))
+        max_abs_force_indices.append(max_abs_force_idx)
 
-def vector_angle(vector1, vector2):
-    dot_product_value = dot_product(vector1, vector2)
-    vector1_length = vector_length(vector1)
-    vector2_length = vector_length(vector2)
+    return max_abs_force_indices
 
-    if vector1_length == 0 or vector2_length == 0:
-        raise ValueError("向量长度不能为0")
+# 示例数据
+force = [1, -2, 3, -4, 3, 2, 1, -2, 3, -4, 3, 2, 0, 1, 2]
+zero_number = [0, 5, 10, 12, 14]
 
-    cos_theta = dot_product_value / (vector1_length * vector2_length)
-    theta = math.acos(cos_theta)
-    theta_degrees = math.degrees(theta)
-    return theta_degrees
-
-if __name__ == "__main__":
-    # 示例向量
-    vector1 = [1, 2, 3]
-    vector2 = [4, 5, 6]
-
-    # 计算夹角
-    angle = vector_angle(vector1, vector2)
-
-    # 判断是否大于120°
-    if angle > 120:
-        print("夹角大于120°")
-    else:
-        print("夹角不大于120°")
+# 求得每一段上force绝对值最大的点的force序号
+max_abs_force_indices = find_max_abs_force_indices(force, zero_number)
+print("每一段上force绝对值最大的点的force序号：", max_abs_force_indices)
