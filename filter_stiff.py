eeba5dcf419c1d889119b83da6985175d8da604a
憨憨菜 <1607662794@qq.com>
2024-01-23 15:31:49 +0800

    刚度退化抽样文件

--- filter_stiff.py
+++ filter_stiff.py
@@ -15,35 +15,51 @@ filtered_df = df[df['image_names'].isin(image_names_to_filter)]
 
 # 将筛选后的数据保存到新的CSV文件中
 output_file_path = r'E:\Code\Image regression\data\degraded_stiffness.csv'
-filtered_df.to_csv(output_file_path, index=False)
+# filtered_df.to_csv(output_file_path, index=False)
 
 # 绘制散点图
 plt.figure(figsize=(10, 6))
 
+fig, ax = plt.subplots()
+
 # 原图
-plt.scatter(df['cumulative deformation(mm)'], df['degraded stiff(KN/mm)'], label='源数据', marker='o', s=20)
+ax.plot(df['times(s)'], df['degraded stiff(KN/mm)'] / 61.27174006657513, label='Source data',color=(58/255, 9/255, 100/255))
 # plt.scatter(df['cumulative deformation(mm)'], df['degraded stiff(KN/mm)'], label='Original Data', marker='o', s=20)
 
 # 筛选后的图
-plt.scatter(filtered_df['cumulative deformation(mm)'], filtered_df['degraded stiff(KN/mm)'], label='过滤数据', color='red', marker='x', s=50)
+# plt.scatter(filtered_df['times(s)'], filtered_df['degraded stiff(KN/mm)'], label='过滤数据',  c='orange', marker='s', edgecolors=['dimgray'])#原图
+ax.scatter(filtered_df['times(s)'], filtered_df['degraded stiff(KN/mm)'] / 61.27174006657513, label='Filter data',  color=(58/255, 9/255, 100/255),s=30, marker='v',zorder=2)
 # plt.scatter(filtered_df['cumulative deformation(mm)'], filtered_df['degraded stiff(KN/mm)'], label='Filtered Data', color='red', marker='x', s=50)
 
 # 设置标题和标签
-plt.title('Degraded Stiffness')
-plt.xlabel('cumulative deformation(mm)')
-plt.ylabel('degraded stiffness(KN/mm)')
-plt.title('刚度退化')
-plt.xlabel('累计位移(mm)')
-plt.ylabel('刚度(KN/mm)')
+# plt.title('Degraded Stiffness')
+# plt.xlabel('cumulative deformation(mm)')
+# plt.ylabel('degraded stiffness(KN/mm)')
+
+# plt.xlabel('累计位移(mm)')
+ax.set_xlabel('Time(s)', font={'family': 'Times New Roman', 'size': 22})
+ax.set_ylabel('Stiffness degradation ratio', font={'family': 'Times New Roman', 'size': 22})
+
+# 刻度值字体大小设置（x轴和y轴同时设置）
+plt.tick_params(labelsize=20)
 
 # 添加网格线
-plt.grid(True, linestyle='--', alpha=0.5)
+xiloc = plt.MultipleLocator(250)  # Set a tick on each integer multiple of the *base* within the view interval.
+yiloc = plt.MultipleLocator(0.1)  # Set a tick on each integer multiple of the *base* within the view interval.
+ax.xaxis.set_minor_locator(xiloc)
+ax.yaxis.set_minor_locator(yiloc)
+ax.grid(color='lightgray', linestyle='-', linewidth=0.5, axis='both', which='both')
 
 # 添加图例
-plt.legend()
+plt.legend(prop={'family': 'Times New Roman', 'size': 22})
+
+# 移动轴的边缘来为刻度标注腾出空间
+plt.tight_layout()
 
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
 plt.rcParams['axes.unicode_minus'] = False
+plt.savefig(f'E:\Code\Image regression\论文绘图\python绘图保存\stiff抽样图.png', dpi=600, bbox_inches='tight')
+
 
 # 显示图形
 plt.show()
