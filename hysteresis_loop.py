7b805fa2428f8ab1144efa4589d66f7617cbd4f6
憨憨菜 <1607662794@qq.com>
2024-01-23 15:31:30 +0800

    滞回圈文件

--- hysteresis_loop.py
+++ hysteresis_loop.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 
 
-def plot_hysteresis_loop(force, displace, zero_number,reverse_disp,reverse_force, switch=True):
+def plot_hysteresis_loop(force, displace, zero_number, reverse_disp, reverse_force, switch=True):
     if switch:  # switch控制是否开启多图显示
         num_loops = len(zero_number) - 1
         '''在多张图中显示'''
@@ -26,27 +26,38 @@ def plot_hysteresis_loop(force, displace, zero_number,reverse_disp,reverse_force
             if switch:
                 plt.scatter(reverse_disp, reverse_force)
             plt.show()
+            plt.savefig(f'E:\Code\Image regression\论文绘图\python绘图保存\滞回圈.png', dpi=600, bbox_inches='tight')
     else:
         '''在一张图中显示'''
         num_loops = len(zero_number)
 
+        fig, ax = plt.subplots()
+
         for i in range(num_loops):
             start_idx = 0 if i == 0 else zero_number[i - 1]
             end_idx = zero_number[i]
+            ax.plot(displace[start_idx:end_idx + 1], force[start_idx:end_idx + 1], label=f'Loop {i + 1}',
+                    color=f'{(i / num_loops) / 1.2}')
 
-            plt.plot(displace[start_idx:end_idx + 1], force[start_idx:end_idx + 1], label=f'Loop {i + 1}')
-
-        # plt.xlabel('Displacement')
-        # plt.ylabel('Force')
-        # plt.title('Hysteresis Loop')
-        plt.xlabel('位移(mm)')
-        plt.ylabel('力(KN)')
-        plt.title('滞回圈')
-        plt.legend(fontsize='small')  # 设置图例字体大小为'small'
-        plt.grid(True)
+        ax.set_xlabel('Displacement (mm)', font={'family': 'Times New Roman', 'size': 20})
+        ax.set_ylabel('Force (kN)', font={'family': 'Times New Roman', 'size': 20})
+        # plt.title('滞回圈')
+        plt.legend(prop={'size': 8.2}, loc=7)  # 设置图例字体大小为'small'
+        xiloc = plt.MultipleLocator(2.5)  # Set a tick on each integer multiple of the *base* within the view interval.
+        yiloc = plt.MultipleLocator(10)  # Set a tick on each integer multiple of the *base* within the view interval.
+        ax.xaxis.set_minor_locator(xiloc)
+        ax.yaxis.set_minor_locator(yiloc)
+        ax.grid(color='lightgray', linestyle='-', linewidth=0.5, axis='both', which='both')
         plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
         plt.rcParams['axes.unicode_minus'] = False
+        # 刻度值字体大小设置（x轴和y轴同时设置）
+        plt.tick_params(labelsize=18)
+        # 移动轴的边缘来为刻度标注腾出空间
+        plt.tight_layout()
+        plt.savefig(f'E:\Code\Image regression\论文绘图\python绘图保存\滞回圈.png', dpi=600, bbox_inches='tight')
         plt.show()
+
+
 
 
 if __name__ == "__main__":
